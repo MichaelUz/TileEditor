7ed8dd3b9c1cca0b8f2512b4d368e8b12817254d
mike <nouzoegwu@outlook.com>
2020-07-16 13:32:36 -0400

    Tweaked the login page css

--- tile_editor_app/src/App.js
+++ tile_editor_app/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 
 import LoginBox from './components/LoginBox/LoginBox';
-import GitHubLogo from './assets/images/GitHub-Mark-64px.png'
+import GitHubLogo from './assets/images/GitHub-Mark-64px.png';
 
 function App() {
   return (
     <div className="App">
-     <h1 className = 'TitleText'>Tiles</h1>
-     <LoginBox/>
-     <a href={'https://github.com/MichaelUz/TileEditor'} target="_blank" rel="noopener noreferrer">
-      <img className='githubLogo' src= {GitHubLogo}/>
-     </a>
+      <text className = 'TitleText'>Tiles</text>
+      <hr/>
+      <LoginBox/>
+      <a href={'https://github.com/MichaelUz/TileEditor'} target="_blank" rel="noopener noreferrer">
+        <img className='githubLogo' src= {GitHubLogo}/>
+      </a>
     </div>
   );
 }
